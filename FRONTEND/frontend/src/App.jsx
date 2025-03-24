ab95b6742e9170a6b6b1ba2d31026c46b5058fbd
QUASAR <mukungavi344@gmail.com>
2025-03-24 18:19:23 +0300

    Removed unnecessary files

--- FRONTEND/frontend/src/App.jsx
+++ FRONTEND/frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import AppointmentCalendar from "./Appointments.jsx"; // Importing the Appointments component
-
+import Dashboard from "./Dashboard.jsx";
+import Home from "./Home.jsx"
+import AppointmentDetails from "./AppointmentDetails.jsx";
+import AppointmentsList from "./AppointmentsList.jsx";
 const App = () => {
   return (
     <div>
-      <AppointmentCalendar />
+      <AppointmentsList/>
     </div>
   );
 };
